ce824cc369b9685929cb4bee7d5248ad7f63ad89
iiwwnnaa <iwna77@naver.com>
2020-07-02 18:33:47 +0900

    fix code

--- omok.c
+++ omok.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 #include <Windows.h>
 #include <conio.h>
-#define SIZE 19
+#define SIZE 19 // SIZE는 가변
 int board[SIZE][SIZE] = { 0 };
 void gameInit();
 void gotoxy(int x, int y);
 void setStone(int stoneColor);
 BOOL checkGameover();
-COORD getCursorXY();
 
 int main() {
 	gameInit(); //게임 보드 그리기
@@ -17,7 +16,7 @@ int main() {
 			gotoxy(1, SIZE);
 			printf("Black Turn");
 		}
-		else { // 이외는 하얀돌
+		else { // 이외는 흰돌
 			gotoxy(1, SIZE);
 			printf("White Turn");
 		}
@@ -72,8 +71,8 @@ void gameInit() {
 	printf("\n");
 }
 void setStone(int stoneColor) {
-	gotoxy(0, 0);
-	COORD Cursor = getCursorXY();
+	COORD Cursor = { 0,0 };
+	gotoxy(Cursor.Y, Cursor.X);
 	while (1) {
 		char Key = _getch();
 		if (Key == 75) { //좌
@@ -97,15 +96,23 @@ void setStone(int stoneColor) {
 			}
 		}
 		else if (Key == 13) {
-			if (stoneColor % 2 == 0) { // stoneColor가 짝수면 흑색
-				printf("○");
-				board[Cursor.Y][Cursor.X] = 1; //게임 보드에 흑색 누적
+			if (board[Cursor.Y][Cursor.X] == 0) {
+				if (stoneColor % 2 == 0) { // stoneColor가 짝수면 흑돌
+					printf("○");
+					board[Cursor.Y][Cursor.X] = 1; //게임 보드에 흑돌 누적
+				}
+				else {
+					printf("●");	// 이외는 흰돌
+					board[Cursor.Y][Cursor.X] = 2; //게임 보드에 흰돌 누적
+				}
+				gotoxy(1, SIZE + 1);
+				printf("             ");
+				break;
 			}
-			else {
-				printf("●");	// 이외는 하얀색
-				board[Cursor.Y][Cursor.X] = 2; //게임 보드에 하얀색 누적
+			else { // 중복돌 방지
+				gotoxy(1, SIZE + 1);
+				printf("Already exist");
 			}
-			break;
 		}
 		gotoxy(Cursor.X, Cursor.Y); // 커서 움직임
 	}
@@ -115,19 +122,16 @@ void gotoxy(int x, int y) {
 	COORD pos = { x * 2,y };
 	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos);
 }
-COORD getCursorXY() {
-	COORD Pos;
-	CONSOLE_SCREEN_BUFFER_INFO a;
-	GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &a);
-	Pos.X = a.dwCursorPosition.X;
-	Pos.Y = a.dwCursorPosition.Y;
-	return Pos;
-}
+
 BOOL checkGameover() {
+	int count = 0;
 	for (int y = 0; y < sizeof(board) / sizeof(board[y]); y++) { // Board의 세로 크기만큼 배열 반복
 		for (int x = 0; x < sizeof(board[0]) / sizeof(int); x++) { //Board의 가로 크기만큼 배열 반복
 			int cases[4] = { 0 }; // 4개의 케이스 누적 전용 변수
 			int color = board[y][x]; // 현재 위치의 돌 색 받아옴
+			if (color == 1 || color == 2) {
+				count++;
+			}
 			for (int i = 0; i < 5 && color != 0; i++) { //5번 반복
 				if (x + i < SIZE && color == board[y][x + i]) { // 돌 옆에 같은색의 돌이 있으면
 					cases[0]++;
@@ -143,14 +147,18 @@ BOOL checkGameover() {
 				}
 			}
 			for (int i = 0; i < 4; i++) { // 누적된 케이스를 순회
+				gotoxy(1, SIZE); 
 				if (cases[i] == 5) { // 케이스중 오목의 조건이 일치하면
-					gotoxy(1, SIZE); // 우승 출력
 					if (color == 1) {
 						printf("Black Win!");
 					}
 					else {
 						printf("White Win!");
 					}
+					return TRUE;
+				}
+				else if (count >= SIZE*SIZE) { // 보드가 돌로 꽉차면 비김
+					printf("DRAW!    ");
 					return TRUE;
 				}
 			}
